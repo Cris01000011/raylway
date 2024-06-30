8a11dc4c41fa2a6a32db5f625e003b2a28941847
Cris01000011 <168916184+Cris01000011@users.noreply.github.com>
2024-06-30 17:01:22 -0600

    raylway

---
+++ src/main/java/com/example/raylway/NombreController.java
@@ -0,0 +1,13 @@
+package com.example.raylway;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class NombreController {
+
+    @GetMapping("/nombre")
+    public String getNombre() {
+        return "Tu Nombre";
+    }
+}
